3c9384938296f4a9b004638774eb010c5acaff86
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-05-06 23:50:38 +0200

    Add setup.py with basic project info

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+from poll import project_info
+
+setup(
+    name=project_info.name,
+
+    use_scm_version=True,
+
+    description=project_info.description,
+
+    url=project_info.url,
+
+    author=project_info.author_name,
+    author_email=project_info.author_email,
+
+    license=project_info.license_name,
+
+    packages=find_packages(),
+
+    setup_requires=[
+        'setuptools_scm'
+    ],
+
+    install_requires=[
+    ],
+
+    python_requires='>=3',
+)
