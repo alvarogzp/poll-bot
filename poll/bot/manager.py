1a11c9e1a152284caf081ed4703de4e8bd6d34d8
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-05-06 23:58:38 +0200

    Wrap all actions with an AsyncApiAction

--- poll/bot/manager.py
+++ poll/bot/manager.py
@@ -8,6 +8,7 @@ from bot.action.standard.admin.fail import FailAction
 from bot.action.standard.admin.instance import InstanceAction
 from bot.action.standard.admin.state import StateAction
 from bot.action.standard.answer import AnswerAction
+from bot.action.standard.async import AsyncApiAction
 from bot.action.standard.asynchronous import AsynchronousAction
 from bot.action.standard.benchmark import BenchmarkAction, WorkersAction
 from bot.action.standard.info.action import UserInfoAction
@@ -27,113 +28,115 @@ class BotManager:
         self.bot.set_action(
             ActionGroup(
                 LoggerAction(reuse_max_length=2000, reuse_max_time=1, reuse_max_number=10).then(
+                    AsyncApiAction().then(
 
-                    MessageAction().then(
-                        PerChatAction().then(
+                        MessageAction().then(
+                            PerChatAction().then(
 
-                            InternationalizationAction().then(
-                                TextMessageAction().then(
+                                InternationalizationAction().then(
+                                    TextMessageAction().then(
 
-                                    CommandAction("about").then(
-                                        AboutAction(
-                                            project_info.name,
-                                            authors=project_info.authors_credits,
-                                            is_open_source=project_info.is_open_source,
-                                            url=project_info.url,
-                                            license_name=project_info.license_name,
-                                            license_url=project_info.license_url,
-                                            donation_addresses=project_info.donation_addresses
-                                        )
-                                    ),
+                                        CommandAction("about").then(
+                                            AboutAction(
+                                                project_info.name,
+                                                authors=project_info.authors_credits,
+                                                is_open_source=project_info.is_open_source,
+                                                url=project_info.url,
+                                                license_name=project_info.license_name,
+                                                license_url=project_info.license_url,
+                                                donation_addresses=project_info.donation_addresses
+                                            )
+                                        ),
 
-                                    CommandAction("version").then(
-                                        VersionAction(
-                                            project_info.name,
-                                            project_info.url + "/releases"
-                                        )
-                                    ),
+                                        CommandAction("version").then(
+                                            VersionAction(
+                                                project_info.name,
+                                                project_info.url + "/releases"
+                                            )
+                                        ),
 
-                                    CommandAction("me", is_personal=True).then(
-                                        UserInfoAction(always_sender=True)
-                                    ),
+                                        CommandAction("me", is_personal=True).then(
+                                            UserInfoAction(always_sender=True)
+                                        ),
 
-                                    CommandAction("benchmark").then(
-                                        AdminActionWithErrorMessage().then(
-                                            AsynchronousAction("benchmark").then(
-                                                BenchmarkAction()
+                                        CommandAction("benchmark").then(
+                                            AdminActionWithErrorMessage().then(
+                                                AsynchronousAction("benchmark").then(
+                                                    BenchmarkAction()
+                                                )
+                                            )
+                                        ),
+                                        CommandAction("restart").then(
+                                            AdminActionWithErrorMessage().then(
+                                                RestartAction()
+                                            )
+                                        ),
+                                        CommandAction("halt").then(
+                                            AdminActionWithErrorMessage().then(
+                                                HaltAction()
+                                            )
+                                        ),
+                                        CommandAction("eval").then(
+                                            AdminActionWithErrorMessage().then(
+                                                EvalAction()
+                                            )
+                                        ),
+                                        CommandAction("state").then(
+                                            AdminActionWithErrorMessage().then(
+                                                StateAction()
+                                            )
+                                        ),
+                                        CommandAction("config").then(
+                                            AdminActionWithErrorMessage().then(
+                                                ConfigStatusAction()
+                                            )
+                                        ),
+                                        CommandAction("instance").then(
+                                            AdminActionWithErrorMessage().then(
+                                                InstanceAction()
+                                            )
+                                        ),
+                                        CommandAction("workers").then(
+                                            AdminActionWithErrorMessage().then(
+                                                WorkersAction()
+                                            )
+                                        ),
+                                        CommandAction("fail").then(
+                                            AdminActionWithErrorMessage().then(
+                                                FailAction()
                                             )
                                         )
-                                    ),
-                                    CommandAction("restart").then(
-                                        AdminActionWithErrorMessage().then(
-                                            RestartAction()
-                                        )
-                                    ),
-                                    CommandAction("halt").then(
-                                        AdminActionWithErrorMessage().then(
-                                            HaltAction()
-                                        )
-                                    ),
-                                    CommandAction("eval").then(
-                                        AdminActionWithErrorMessage().then(
-                                            EvalAction()
-                                        )
-                                    ),
-                                    CommandAction("state").then(
-                                        AdminActionWithErrorMessage().then(
-                                            StateAction()
-                                        )
-                                    ),
-                                    CommandAction("config").then(
-                                        AdminActionWithErrorMessage().then(
-                                            ConfigStatusAction()
-                                        )
-                                    ),
-                                    CommandAction("instance").then(
-                                        AdminActionWithErrorMessage().then(
-                                            InstanceAction()
-                                        )
-                                    ),
-                                    CommandAction("workers").then(
-                                        AdminActionWithErrorMessage().then(
-                                            WorkersAction()
-                                        )
-                                    ),
-                                    CommandAction("fail").then(
-                                        AdminActionWithErrorMessage().then(
-                                            FailAction()
-                                        )
-                                    )
 
+                                    )
                                 )
                             )
-                        )
-                    ),
+                        ),
 
-                    NoPendingAction().then(
-                        MessageAction().then(
-                            PerChatAction().then(
-                                TextMessageAction().then(
-                                    CommandAction("ping").then(
-                                        AnswerAction("Up and running!")
+                        NoPendingAction().then(
+                            MessageAction().then(
+                                PerChatAction().then(
+                                    TextMessageAction().then(
+                                        CommandAction("ping").then(
+                                            AnswerAction("Up and running!")
+                                        )
+                                    )
+                                )
+                            )
+                        ),
+
+                        PendingAction().then(
+                            MessageAction().then(
+                                PerChatAction().then(
+                                    TextMessageAction().then(
+                                        CommandAction("ping").then(
+                                            AnswerAction("I'm back! Sorry for the delay...")
+                                        )
                                     )
                                 )
                             )
                         )
-                    ),
 
-                    PendingAction().then(
-                        MessageAction().then(
-                            PerChatAction().then(
-                                TextMessageAction().then(
-                                    CommandAction("ping").then(
-                                        AnswerAction("I'm back! Sorry for the delay...")
-                                    )
-                                )
-                            )
-                        )
                     )
-
                 )
             )
         )
