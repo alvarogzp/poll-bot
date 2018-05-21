1734b57cea166a0983e4fb4aee474674568baf95
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-05-21 12:07:38 +0200

    Create PollNumber

--- poll/domain/model/poll/poll.py
+++ poll/domain/model/poll/poll.py
@@ -5,3 +5,8 @@ class Poll(Comparable):
     def __init__(self, poll_id: int):
         super().__init__(poll_id, Poll)
         self.id = poll_id
+
+
+class PollNumber:
+    def __init__(self, number: int):
+        self.number = number
