96ac02f2fcd203ac2ee6265bdf04e8744757de05
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-05-23 12:07:38 +0200

    Create UserStateInteractor in new interactors.user package

---
+++ poll/domain/interactors/user/state.py
@@ -0,0 +1,14 @@
+from poll.domain.model.user.state import State
+from poll.domain.model.user.user import User
+from poll.domain.repository.state import UserStateRepository
+
+
+class UserStateInteractor:
+    def __init__(self, state: UserStateRepository):
+        self.state = state
+
+    def set_state(self, user: User, state: State):
+        self.state.set_state(user, state)
+
+    def get_state(self, user: User):
+        return self.state.get_state(user)
