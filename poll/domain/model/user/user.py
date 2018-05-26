9d5b9ab57cc53775e68e88664738ab7e085f1705
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-05-26 16:07:38 +0200

    Create AnonymousUser

--- poll/domain/model/user/user.py
+++ poll/domain/model/user/user.py
@@ -5,3 +5,8 @@ class User(Comparable):
     def __init__(self, user_id: int):
         super().__init__(user_id, User)
         self.id = user_id
+
+
+class AnonymousUser(User):
+    def __init__(self, anonymized_user_id: int):
+        super().__init__(anonymized_user_id)
