a8cf5e30f672d2ef18a2d4b7701d1688c41db83c
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-05-26 08:37:38 +0200

    Create LoggerInjector

---
+++ poll/inject/injector/presentation/telegram/logger.py
@@ -0,0 +1,20 @@
+from bot.logger.logger import Logger
+
+from poll.domain.logger import PollLogger
+from poll.inject.injector.base import BaseInjector
+from poll.inject.injector.cache import InjectorCache
+from poll.inject.injector.presentation.telegram.formatter import FormatterInjector
+from poll.presentation.telegram.bot.view.logger import TelegramPollLogger
+
+
+class LoggerInjector(BaseInjector):
+    def __init__(self, cache: InjectorCache, formatter: FormatterInjector, logger: Logger):
+        super().__init__(cache)
+        self.formatter = formatter
+        self.logger = logger
+
+    def poll(self) -> PollLogger:
+        return self._cache(PollLogger, lambda: TelegramPollLogger(
+            self.logger,
+            self.formatter.log()
+        ))
