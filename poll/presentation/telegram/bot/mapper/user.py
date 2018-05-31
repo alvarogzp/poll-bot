d0be88a649729bfaf0db10c03874dc8c496854fe
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-05-31 08:08:36 +0200

    Create a map_user method on UserViewModelMapper

--- poll/presentation/telegram/bot/mapper/user.py
+++ poll/presentation/telegram/bot/mapper/user.py
@@ -5,6 +5,16 @@ from poll.presentation.model.user import UserViewModel
 
 class UserViewModelMapper:
     @staticmethod
+    def map_user(user: UserViewModel) -> ApiObject:
+        return ApiObject(
+            id=user.id,
+            username=user.username,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            language_code=user.language
+        )
+
+    @staticmethod
     def unmap_user(user: ApiObject) -> UserViewModel:
         return UserViewModel(
             user.id,
