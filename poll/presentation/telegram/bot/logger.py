370c7f9ff7a89787678e2025467ae8e4aae7f207
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-05-31 08:23:02 +0200

    Update TelegramPollLogger to use new RepositoryLogFormatter

--- poll/presentation/telegram/bot/logger.py
+++ poll/presentation/telegram/bot/logger.py
@@ -2,19 +2,22 @@ from bot.action.util.textformat import FormattedText
 from bot.logger.logger import Logger
 
 from poll.domain.logger import PollLogger
-from poll.presentation.telegram.bot.formatter.log import LogFormatter
+from poll.presentation.telegram.bot.formatter.log.repository import RepositoryLogFormatter
 
 
 LOG_TAG_MIGRATION = FormattedText().bold("MIGRATION")
 
 
 class TelegramPollLogger(PollLogger):
-    def __init__(self, logger: Logger, log_formatter: LogFormatter):
+    def __init__(self, logger: Logger, repository_log_formatter: RepositoryLogFormatter):
         self.logger = logger
-        self.log_formatter = log_formatter
+        self.repository_log_formatter = repository_log_formatter
 
     def repository_component_migration(self, component: str, migration_type: str, old_version: int, new_version: int,
                                        about_to_migrate_to_version: int):
-        self.logger.log(LOG_TAG_MIGRATION, self.log_formatter.repository_component_migration(
-            component, migration_type, old_version, new_version, about_to_migrate_to_version
-        ))
+        self.logger.log(
+            LOG_TAG_MIGRATION,
+            self.repository_log_formatter.repository_component_migration(
+                component, migration_type, old_version, new_version, about_to_migrate_to_version
+            )
+        )
