1df40b78ecfdbc685dc4352cfc5b5b95f93a6329
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-06-01 11:01:12 +0200

    Use UserSettingsInteractor in BotManagePollInteractor to create new polls with the current user settings, and to return them on start

--- poll/domain/interactors/bot/manage.py
+++ poll/domain/interactors/bot/manage.py
@@ -2,11 +2,12 @@ from typing import Union, Optional
 
 from poll.domain.interactors.poll.get import GetPollInteractor
 from poll.domain.interactors.poll.manage import ManagePollInteractor
+from poll.domain.interactors.user.settings import UserSettingsInteractor
 from poll.domain.interactors.user.state import UserStateInteractor
 from poll.domain.model.poll.info import PollInfo
 from poll.domain.model.poll.option import PollOptionInfo, PollOptionNumber
 from poll.domain.model.poll.poll import PollNumber
-from poll.domain.model.poll.settings.settings import DEFAULT_SETTINGS, PollSettings
+from poll.domain.model.poll.settings.settings import PollSettings
 from poll.domain.model.poll.user import PollUser
 from poll.domain.model.user.state import WAITING_TITLE, INCOMPLETE_POLL_STATES, WAITING_MORE_OPTIONS, IDLE, IDLE_STATES, \
     State, ADDING_OPTIONS_STATES, READY_TO_COMPLETE_STATES, WAITING_FIRST_OPTION
@@ -14,16 +15,19 @@ from poll.domain.model.user.user import User
 
 
 class BotManagePollInteractor:
-    def __init__(self, manage: ManagePollInteractor, get: GetPollInteractor, state: UserStateInteractor):
+    def __init__(self, manage: ManagePollInteractor, get: GetPollInteractor, state: UserStateInteractor,
+                 settings: UserSettingsInteractor):
         self.manage = manage
         self.get = get
         self.state = state
+        self.settings = settings
 
     # PUBLIC INTERFACE
 
     def start(self, user: User) -> PollSettings:
         self._cancel(user, new_state=WAITING_TITLE)
-        return DEFAULT_SETTINGS
+        settings = self.settings.get(user)
+        return settings.poll_settings
 
     def message(self, user: PollUser, text: str) -> Union[PollNumber, PollOptionNumber, None]:
         state = self.state.get_state(user)
@@ -38,7 +42,8 @@ class BotManagePollInteractor:
             return number
 
     def _new_poll(self, user: PollUser, title: str) -> PollNumber:
-        info = PollInfo(user, DEFAULT_SETTINGS, title, False)
+        settings = self.settings.get(user)
+        info = PollInfo(user, settings.poll_settings, title, False)
         return self.manage.new_poll(info)
 
     def _option(self, user: User, option: str) -> PollOptionNumber:
